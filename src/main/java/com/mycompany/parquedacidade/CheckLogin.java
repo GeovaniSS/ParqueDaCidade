f5e5de5454c1fdce4cf2ca635ae44c2bc54fbd10
Bernardo Borges <bernardobbm334@gmail.com>
2022-12-18 21:31:26 -0300

    fix: Corrected check

--- src/main/java/com/mycompany/parquedacidade/CheckLogin.java
+++ src/main/java/com/mycompany/parquedacidade/CheckLogin.java
@@ -4,42 +4,20 @@
  */
 package com.mycompany.parquedacidade;
 
-import java.util.regex.Pattern;
-
 /**
  *
  * @author bernardo
  */
 public class CheckLogin {
-    public void validateData(String email, String password){
+    public void validateDataLogin(String email, String password){
         // Email Validation
         if(email.length() < 1) {
             System.out.println("Email field cannot be empty");
-        } else {
-            if(isValidEmailAdress(email)) {
-                System.out.println("Valid email");
-            } else {
-                System.out.println("Invalid email");
-            }
         }
+        
         // Password Validation 
         if(password.length() < 1) {
              System.out.println("Password field cannot be empty");
-        } else {
-            if(password.length() < 6) {
-                System.out.println("Password is too short");
-            } else {
-                 System.out.println("Your password is valid");
-            }
         }
-    }
-    
-    public static boolean isValidEmailAdress(String email) {
-        String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" 
-        + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-        
-        return Pattern.compile(regexPattern)
-            .matcher(email)
-            .matches();
     }
 }
